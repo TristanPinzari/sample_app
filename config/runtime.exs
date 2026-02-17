e9d6293fd900ccf410d8f954750aa78bb8c0c3a0
Tristan <tristanpinzari@example.com>
2026-02-17 17:55:35 -0500

    trying to get s3 working

--- config/runtime.exs
+++ config/runtime.exs
@@ -127,15 +127,12 @@ if config_env() == :prod do
   #
   # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
 
-  config :waffle, :storage_config,
-    s3_config: [
-      access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
-      secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
-      region: System.get_env("AWS_REGION")
-    ]
+config :waffle,
+  storage: Waffle.Storage.S3,
+  bucket: System.get_env("AWS_S3_BUCKET")
 
-  config :ex_aws,
-    access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
-    secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
-    region: System.get_env("AWS_REGION")
+config :ex_aws,
+  access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
+  secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
+  region: System.get_env("AWS_REGION")
 end
